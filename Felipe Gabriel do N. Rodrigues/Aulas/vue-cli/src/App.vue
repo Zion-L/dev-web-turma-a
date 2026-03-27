2d6d5eed94df55ff419cb9dc999c97ffee66d6ff
Kyon-S2 <felipegabriel3356@gmail.com>
2026-03-26 21:22:00 -0300

    crated,mounted,etc

--- Felipe Gabriel do N. Rodrigues/Aulas/vue-cli/src/App.vue
+++ Felipe Gabriel do N. Rodrigues/Aulas/vue-cli/src/App.vue
@@ -2,20 +2,42 @@
 
   <div>
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Meu App Vue</h1>
+    <p class="subTitulo">Meu subtítulo</p>
     <br>
-    <home-component/>
+    <home-component />
+    <br>
+    <usuario-component />
+    <br>
+    <estilo-component />
   </div>
-  
+
 </template>
 
 <script>
 
+import EstiloComponent from './components/EstiloComponent.vue';
 import HomeComponent from './components/HomeComponent.vue';
+import UsuarioComponent from './components/UsuarioComponent.vue';
 
 export default {
   name: 'App',
   components: {
     HomeComponent,
+    UsuarioComponent,
+    EstiloComponent,
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted")
+  },
+  updated() {
+    console.log("updated")
+  },
+  errorCaptured() {
+    console.log("errorCaptured")
   },
 };
 </script>
@@ -28,5 +50,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  color: #2c3e50;
+}
+
+.subTitulo {
+  color: aqua;
+  font-family: "Gill Sans";
+  text-align: center;
 }
 </style>
